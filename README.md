96444dce5b8515c7ef5456f7043d6dac948edf03
Arnaud Gaydamour <arnaud.gaydamour@protonmail.com>
2023-12-07 21:28:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Requis
+
+Dans `node_modules/react-virtualized/dist/es/WindowScroller/utils/onScroll.js`, commenter la ligne:
+```js
+import { bpfrpt_proptype_WindowScroller } from "../WindowScroller.js";
+```
---
+++ src/App.jsx
@@ -0,0 +1,155 @@
+import { useState, useRef } from "react";
+import ReactPlayer from "react-player";
+
+import { Timeline } from '@xzdarcy/react-timeline-editor';
+
+
+const App = () => {
+
+  const [src, setSrc] = useState("");
+
+  const playerRef = useRef();
+
+  const handleChange = (event) => {
+    try {
+      const file = event.target.files[0];
+      setSrc(URL.createObjectURL(file));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleProgress = (state) => {
+    if (timelineRef.current) {
+      autoScroll(state)
+    }
+  };
+
+  // Set the timeline position to show the cursor
+  const autoScroll = (state) => {
+    const timelineWidth = timelineRef.current.target.clientWidth;
+    const timelineCursorPosition = timelineRef.current.target.lastChild.offsetLeft;
+
+    const timePosition = state.playedSeconds * 160;
+    const scrollPosition = timePosition;
+    if (timelineCursorPosition > (timelineWidth - 160)) {
+      timelineRef.current.setScrollLeft(scrollPosition);  
+    } else if (timelineCursorPosition < 0) {
+      timelineRef.current.setScrollLeft(scrollPosition);
+    }
+  }
+
+  const [mockData, setMockData] = useState([
+    // Il faut ajouter un element de fin non modifiable pour la frise
+    // La classe de cet element c'est '.timeline-editor-action-effect-effect1'
+    // Il faut faire un display none pour cacher cette action
+    {
+      id: "_",
+      actions: [
+        {
+          id: "endOfTimeline",
+          start: 499.9,
+          end: 500,
+          effectId: "effect1",
+        }
+      ],
+    }
+  ]);
+
+  const mockEffect = {
+    effect0: {
+      id: "effect0",
+      name: "效果0",
+    },
+    effect1: {
+      id: "effect1",
+      name: "效果1",
+    },
+  };
+
+  // Module
+  const onTimeLineChange = (time) => {
+    if (playerRef.current) {
+      playerRef.current.seekTo(time, 'seconds');
+    }
+  }
+
+  const timelineRef = useRef(null);
+
+  const onPlay = () => {
+    const playerTime = playerRef.current.getCurrentTime();
+    if (timelineRef.current) {
+      timelineRef.current.setTime(playerTime);
+      timelineRef.current.play();
+    }
+  }
+
+  const onPause = () => {
+    const playerTime = playerRef.current.getCurrentTime();
+    if (timelineRef.current) {
+      timelineRef.current.pause();
+      timelineRef.current.setTime(playerTime);
+    }
+  }
+
+  const onTimelineAddData = () => {
+
+    let data = [...mockData];
+    data[0].actions.push({
+      id: `action_${timelineRef.current.getTime()}`,
+      start: timelineRef.current.getTime(),
+      end: timelineRef.current.getTime() + 2,
+      effectId: "effect0",
+    });
+    setMockData(data);
+
+    // Si le player ne se met pas a jour, la timeline freeze
+    playerRef.current.getInternalPlayer().pause();
+    playerRef.current.getInternalPlayer().play();
+  }
+
+  return (
+    <>
+      {src === '' ||
+        <>
+          <div className="video">
+            <ReactPlayer ref={playerRef} url={src} muted controls onProgress={handleProgress} onPause={onPause} onPlay={onPlay} height="50%" width="auto" /> 
+          </div>
+          <div className="video-controls">
+            {/* <button onClick={showTimestamp}>Show Current Timestamp</button><span><u>Current timestamp:</u> <b>{time}</b></span> */}
+          </div>
+        </>
+      }
+
+      {/* Module */}
+      <Timeline
+        key={mockData.length}
+        ref={timelineRef}
+        editorData={mockData}
+        effects={mockEffect}
+        onCursorDragEnd={(time) => onTimeLineChange(time)}
+        style={{ height: `150px`, width: '100%' }}
+        maxScaleCount={500}
+        scale={1}
+        autoScroll={true}
+        onChange={() => {}}
+        // getActionRender={(action) => {
+        //   if (action.id === 'action10') {
+        //       return <p>Hello</p>
+        //   }
+        // }}
+      />
+
+      <button onClick={onTimelineAddData}>Add Data Current Time</button>
+
+
+      {src !== '' ||
+        <div className="video-controls">
+          <input type="file" onChange={handleChange} />
+        </div>
+      }
+    </>    
+  );
+};
+
+export default App;
